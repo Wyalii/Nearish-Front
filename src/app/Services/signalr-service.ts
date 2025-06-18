529f0436aa06663f5f52edfd49f699ddd67a164b
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-06-18 23:05:55 +0400

    added signalr service (testing phase).

---
+++ src/app/Services/signalr-service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalrService {
+  private chatConnection!: signalR.HubConnection;
+  private notificationsConnection!: signalR.HubConnection;
+
+  // --- CHAT CONNECTION ---
+  startChatConnection(token: string) {
+    this.chatConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://nearish-back.onrender.com/chathub', {
+        accessTokenFactory: () => token
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.chatConnection.start()
+      .then(() => console.log('Chat Hub connected'))
+      .catch(err => console.error('Chat Hub connection error:', err));
+
+    this.chatConnection.onclose(error => {
+      console.warn('Chat connection closed', error);
+    });
+  }
+
+  sendChatMessage(receiverId: string, message: string) {
+    this.chatConnection.invoke('SendMessage', receiverId, message)
+      .catch(err => console.error('SendMessage error:', err));
+  }
+
+  onChatMessageReceived(handler: (senderId: string, message: string) => void) {
+    this.chatConnection.on('ReceiveMessage', handler);
+  }
+
+  onMessageSent(handler: (message: string) => void) {
+    this.chatConnection.on('MessageSent', handler);
+  }
+
+  onMessageFailed(handler: (error: string) => void) {
+    this.chatConnection.on('MessageFailed', handler);
+  }
+
+  stopChatConnection() {
+    if (this.chatConnection) {
+      this.chatConnection.stop();
+    }
+  }
+
+  // --- NOTIFICATIONS CONNECTION ---
+  startNotificationsConnection(token: string) {
+    this.notificationsConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://nearish-back.onrender.com/notificationshub', {
+        accessTokenFactory: () => token
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.notificationsConnection.start()
+      .then(() => console.log('Notifications Hub connected'))
+      .catch(err => console.error('Notifications Hub connection error:', err));
+
+    this.notificationsConnection.onclose(error => {
+      console.warn('Notifications connection closed', error);
+    });
+  }
+
+  onNotificationReceived(handler: (notification: any) => void) {
+    this.notificationsConnection.on('ReceiveNotification', handler);
+  }
+
+  stopNotificationsConnection() {
+    if (this.notificationsConnection) {
+      this.notificationsConnection.stop();
+    }
+  }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './Services/api-service';
 import { register } from './interfaces/register';
-import { UserRegisterComponent } from "./components/user-register-component/user-register-component";
 import { HeaderComponent } from "./components/header-component/header-component";
+import { SignalrService } from './Services/signalr-service';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +12,27 @@ import { HeaderComponent } from "./components/header-component/header-component"
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  service = inject(ApiService)
+export class App implements OnInit{
   protected title = 'Nearish-Front';
-
-   ragaca!:register
-
-  registerFunc(){
-this.service.register(this.ragaca).subscribe({
-  next:(data)=>{
-    console.log('mushaobs')
-  },
-  error(err){
-    console.log('failed',err)
+  signalRSerivce = inject(SignalrService)
+   ngOnInit(): void {
+    const token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoic3RyaW5nIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiIzIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoibGF3eGcxMjNAZ21haWwuY29tIiwiZXhwIjoxNzUwMjczNDUyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUxNTAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQyMDAifQ.I7J1ePLBK8oQVjslq3xDJcMUBi45drIhwyUsirzXjCaLfsDh7f5xZ7xqMq7V2ehK5Gut7fRRZQvesSapL_TIrw"
+    this.signalRSerivce.startChatConnection(token);
+    this.signalRSerivce.startNotificationsConnection(token);
   }
-})
-  }
+
+  // memgoni agar aris es sawiro xo??
+
+  // service = inject(ApiService)
+  //  ragaca!:register
+  //   registerFunc(){
+  //   this.service.register(this.ragaca).subscribe({
+  //   next:(data)=>{
+  //     console.log('mushaobs')
+  //   },
+  //   error(err){
+  //     console.log('failed',err)
+  //   }
+  // })
+  //   }
 }
